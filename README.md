b1f62ffa40f46cc6969a048a4821d1c0f81c1dd6
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-01-23 18:48:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Projeto E-Mkt
+
+Este projeto dispara e-mails em massa para uma lista de e-mails cadastrado no banco de dados.
+
+## Instalação do Banco de Dados
+
+ - Crie uma base de dados mySql com o nome que preferir. Ex: emailmkt
+ - Importe o arquivo e-mkt.sql no Banco de Dados criado
+
+## Arquivo de configurações gerais
+
+Altere as variáveis do arquivo config.php dentro da pasta includes, para as configurações do Banco de Dados e outras configurações gerais do servidor.
+
+<br /><br />
+
+# ATENÇÃO: ESTE É UM PROJETO LEGADO 
+## PROGRAMAS E VERSÕES UTILIDADAS
+ - PHP versão 5.4.7 ou inferior
+ - MySQL v4.0
---
+++ includes/config.php
@@ -0,0 +1,36 @@
+<?php
+//@session_start();
+//$_SESSION['ssv_usr_tipo'] = 1;
+//$_SESSION['ssv_usr_id'] = 1;
+
+//define('URL', 'http://localhost/php/www/test/ssv/');
+define('URL','http://rafasantoni.heliohost.org/EMkt/');
+define('DIR', '/home/rafasan/public_html/EMkt/');
+
+//echo DIR;
+//echo '<a href="">'.DIR.'</a>';
+
+/* conexao */
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB_NAME', 'emailmkt');
+
+/* PASTAS  */
+define('JS', 'js/');
+define('CSS', 'CSS/');
+define('IMG', 'img/');
+define('INCLUDES', 'includes/');
+define('MODULES', 'modulos/');
+define('CLASSES','classes/');
+define('TEMPLATES','templates/');
+define('FUNCTIONS', 'function/');
+
+//$conn = mysql_connect(HOST, USER, PASS);
+//mysql_select_db(DB_NAME);
+
+
+
+//echo  $conn ? 'Conex�o foi um sucesso' : 'Erro na conex�o';
+
+?>
